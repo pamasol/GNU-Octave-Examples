27c521b1b06aefffc938dcc42089990cbd64eb8b
philippbruhin <info@philippbruhin.ch>
2020-03-29 00:23:30 +0100

    updated sinus plot

--- Simple_Plot.m
+++ Simple_Plot.m
@@ -1,8 +1,17 @@
-x = -10:0.1:10;
+x = 0:0.001:2*pi;
+
+figure (1);
 plot (x, sin (x));
-
+set(gca,'XTick',0:pi/2:2*pi) 
+set(gca,'XTickLabel',{'0','pi/2','pi','3*pi/2','2*pi'})
 xlabel ("x");
-ylabel ("sin (x)");
+ylabel ("sin_rad (x)");
 title ("Simple 2-D Plot");
-
 grid on;
+
+figure (2);
+plot (x, x.^2);
+xlabel ("x");
+ylabel ("x^2");
+title ("Simple 2-D Plot");
+grid minor;
